7776e43975772793a54d523aca27de598c18a499
Garth Lilly <garff247@gmail.com>
2019-05-07 17:32:24 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,16 +2,8 @@ require 'pry'
 class Artist
   attr_accessor :name
 
-  def initialize(name)
-    @name = name
-  end
-
-end
-class Song
-  attr_accessor :title, :artist
-
-  def initialize(title)
-    @title = title
+  def initialize
+    
   end
 
 end
